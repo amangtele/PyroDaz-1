0a967b4ada90033d886e6646bfd2c37dee164c10
GtoPrograM <124225788+FadRepo@users.noreply.github.com>
2023-02-06 22:38:53 +0700

    Update inline.py

--- PyroDaz/modules/bot/inline.py
+++ PyroDaz/modules/bot/inline.py
@@ -63,12 +63,10 @@ async def alive_function(message: Message, answers):
             title="Alive",
             description="Check Bot's Stats",
             thumb_url="https://te.legra.ph/file/2a9123e82f02807224fe7.jpg",
-            input_message_content=InputTextMessageContent(
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
              ),
-            reply_markup=InlineKeyboardMarkup(
+            input_reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("ʜᴇʟᴘ", callback_data="helper")]]
-            ),
         )
     return answers
 
