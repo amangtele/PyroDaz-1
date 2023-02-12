b9d2b60c29971dd9d3a5123a634d134beb074313
GtoPrograM <124225788+FadRepo@users.noreply.github.com>
2023-02-12 12:48:08 +0700

    Update alive.py

--- PyroDaz/modules/alive.py
+++ PyroDaz/modules/alive.py
@@ -144,26 +144,4 @@ async def setemoji(client: Client, message: Message):
     sql.addgvar("ALIVE_EMOJI", emoji)
     await Man.edit(f"**Berhasil Mengcustom EMOJI ALIVE Menjadi** {emoji}")
     restart()
-
-
-add_command_help(
-    "alive",
-    [
-        [
-            "alive",
-            "Untuk memeriksa userbot anda berfungsi atau tidak",
-        ],
-        [
-            "setalivelogo <link telegraph atau reply ke foto/video/gif>",
-            "Untuk mengcustom alive logo userbot anda",
-        ],
-        [
-            "setalivetext <text>",
-            "Untuk mengcustom alive text userbot anda",
-        ],
-        [
-            "setemoji <emoji>",
-            "Untuk mengcustom emoji alive userbot anda",
-        ],
-    ],
-)
+    
