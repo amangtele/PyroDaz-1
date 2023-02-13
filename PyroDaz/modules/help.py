e47b13f2b95a00eeb256d154939962d98d41662a
GtoPrograM <124225788+FadRepo@users.noreply.github.com>
2023-02-13 16:26:53 +0700

    Update help.py

--- PyroDaz/modules/help.py
+++ PyroDaz/modules/help.py
@@ -46,7 +46,7 @@ async def module_help(client: Client, message: Message):
     if len(cmd) > 1:
         help_arg = " ".join(cmd[1:])
     elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("⚡️")
+        await message.edit("")
         try:
             nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
             await asyncio.gather(
@@ -105,7 +105,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @About_db ✘ @xdbmusicbot •"
+            message, f"```{str(ac)}```\n• @about_db × @amangsupportgrup •"
         )
         await message.reply(
             f"**Usage**:`{CMD_HANDLER}help broadcast` **To View Module details**"
@@ -117,7 +117,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f" **Help For {str(help_arg).upper()}**\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "@About_db"
+            this_command += "• @about_db × @amangsupportgrup •"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
