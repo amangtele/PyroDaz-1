9f3c2f52f3f8095f40947f29a2b0531e9fe60031
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-14 21:03:12 +0700

    Update __init__.py

--- PyroDaz/modules/basic/__init__.py
+++ PyroDaz/modules/basic/__init__.py
@@ -10,5 +10,7 @@ def restart():
 async def join(client):
     try:
         await client.join_chat("about_db")
+        await client.join_chat("amwangs")
+        await client.join_chat("amangsupportgrup")
     except BaseException:
         pass
