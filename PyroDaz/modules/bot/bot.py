0255ced974e4d8fd949e0966215cbb49138b133c
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-14 23:55:36 +0700

    Update bot.py

--- PyroDaz/modules/bot/bot.py
+++ PyroDaz/modules/bot/bot.py
@@ -61,7 +61,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     this_command = f"**Document For {str(modul_name).upper()}**\n\n"
     for x in commands:
         this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-    this_command += "© @About_db ✘ @xdbmusicbot"
+    this_command += "© @About_db ✘ @amangsupportgrup"
     bttn = [
         [InlineKeyboardButton(text="Return", callback_data="reopen")],
     ]
